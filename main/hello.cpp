d8a9449b4125cd89071b96a6b27b3d751e51a769
gitnavya0 <navya2govil@gmail.com>
2024-03-06 14:53:20 +0530

    added files

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins PES1UG21CS368\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
